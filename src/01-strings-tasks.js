7a8c14a0990f6804ca2f1faffcb22c99285e9638
VEKozlov111 <vlad.kozlov.2015@gmail.com>
2022-12-19 21:26:00 +0300

    test

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -173,8 +173,8 @@ function unbracketTag(str) {
  *   'Thunderstruck' => 'THUNDERSTRUCK'
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
-function convertToUpperCase(/* str */) {
-  throw new Error('Not implemented');
+function convertToUpperCase(str) {
+  return str.toUpperCase();
 }
 
 /**
